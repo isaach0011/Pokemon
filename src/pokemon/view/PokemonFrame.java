7894403b8a98b4dbcc50d7810fdc27d57d248176
isaach0011 <ihil17523@740s-pl-2.local>
2016-12-01 13:06:45 -0700

    Finished Frame class and started Panel class.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -22,7 +22,7 @@ public class PokemonFrame extends JFrame
 	{
 		this.setContentPane(basePanel);
 		this.setTitle("Pokemon");
-		this.setSize(new Dimension(600,400));
+		this.setSize(new Dimension(900,600));
 		this.setResizable(false);
 		this.setVisible(true);;
 	}
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,100 @@
 package pokemon.view;
 
-public class PokemonPanel 
-{
+import pokemon.controller.PokemonController;
 
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private SpringLayout baseLayout;
+	private ImageIcon pokemonIcon;
+	private JButton updateButton;
+	private JComboBox pokedexSelector;
+	private JLabel pokemonLabel;
+	private JLabel healthLabel;
+	private JLabel combatLabel;
+	private JLabel speedLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel advancedLabel;
+	private JTextField healthField;
+	private JTextField combatField;
+	private JTextField speedField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextArea advancedArea;
+	
+	public PokemonPanel(PokemonController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		this.pokemonIcon = new ImageIcon(getClass().getResource(""));
+		this.updateButton = new JButton("Update the stats!");
+		this.nameField = new JTextField(25);
+		this.combatField = new JTextField(5);
+		this.healthField = new JTextField(5);
+		this.speedField = new JTextField(5);
+		this.numberField = new JTextField(5);
+		this.advancedArea = new JTextArea(10,25);
+		this.pokedexSelector = new JComboBox(new String [] {"Charmander", "Cyndaquil", "Ludicolo", "Snivy{", "Totodile"});
+		this.advancedLabel = new JLabel("Advanced Information");
+		this.combatLabel = new JLabel("Combat Points:");
+		this.healthLabel = new JLabel("Health Points:");
+		this.speedLabel = new JLabel("Speed Poonts:");
+		this.numberLabel = new JLabel("Pokemon Number:");
+		this.nameLabel = new JLabel("Name:");
+		this.pokemonLabel = new JLabel("The current pokemon", pokemonIcon, JLabel.CENTER);
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	public void setupPanel()
+	{
+		this.setLayout(baseLayout);;
+		this.setPreferredSize(new Dimension(900, 600));
+		this.setBackground(Color.DARK_GRAY);
+		
+		numberField.setEditable(false);
+		advancedArea.setEditable(false);
+		advancedArea.setWrapStyleWord(true);
+		advancedArea.setLineWrap(true);
+		
+		pokemonLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		pokemonLabel.setHorizontalTextPosition(JLabel.CENTER);
+		
+		this.add(pokedexSelector);
+		this.add(healthField);
+		this.add(healthLabel);
+		this.add(combatField);
+		this.add(combatLabel);
+		this.add(speedField);
+		this.add(speedLabel);
+		this.add(nameField);
+		this.add(nameLabel);
+		this.add(advancedLabel);
+		this.add(advancedArea);
+		this.add(pokemonLabel);
+		this.add(updateButton);
+	}
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	public void setupListeners()
+	{
+		
+	}
 }
+
+
