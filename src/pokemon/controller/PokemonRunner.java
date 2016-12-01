db47a72823e18c942c654f3abeeeb41a39c0cf8b
isaach0011 <ihil17523@740s-pl-2.local>
2016-12-01 12:32:01 -0700

    Created Project and other things.
    Create runner controller panel and started on the frame class.

---
+++ src/pokemon/controller/PokemonRunner.java
@@ -0,0 +1,11 @@
+package pokemon.controller;
+
+public class PokemonRunner 
+{
+	public static void main (String [] args)
+	{
+		PokemonController poketroller = new PokemonController();
+		poketroller.start();
+		
+	}
+}
---
+++ src/pokemon/view/PokemonFrame.java
@@ -0,0 +1,29 @@
+package pokemon.view;
+
+import javax.swing.JFrame;
+import pokemon.controller.PokemonController;
+import java.awt.Dimension;
+
+public class PokemonFrame extends JFrame
+{
+
+	private PokemonController baseController;
+	private PokemonPanel basePanel;
+	
+	public PokemonFrame(PokemonController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		basePanel = new PokemonPanel(baseController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setTitle("Pokemon");
+		this.setSize(new Dimension(600,400));
+		this.setResizable(false);
+		this.setVisible(true);;
+	}
+}
