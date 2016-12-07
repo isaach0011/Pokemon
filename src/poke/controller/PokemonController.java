66bc1ea98b03751e8a2b1754e3153bff3e3a30ac
isaach0011 <ihil17523@740s-pl-121.local>
2016-12-07 13:46:28 -0700

    Created buildPokedexText method and updateSelected.

--- src/poke/controller/PokemonController.java
+++ src/poke/controller/PokemonController.java
@@ -30,6 +30,26 @@ public class PokemonController
 		pokedex.add(new Charmander());
 	}
 	
+	public String[] buildPokedexText()
+	{
+		String [] pokemonNames = new String[pokedex.size()];
+				
+		for(int index = 0; index < pokedex.size(); index++)
+		{
+			pokemonNames[index] = pokedex.get(index).getName();
+		}
+		
+		return pokemonNames;
+	}
+	
+	public void updateSelected(int index, String name, int combat, int health, double speed)
+	{
+		Pokemon current = pokedex.get(index);
+		current.setName(name);
+		current.setAttackPoints(combat);;
+		current.setSpeed(speed);
+		current.setHealthPoints(health);
+	}
 	public ArrayList<Pokemon> getPokedex()
 	{
 		return pokedex;
